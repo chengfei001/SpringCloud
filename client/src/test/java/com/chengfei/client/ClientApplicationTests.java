a0d55ee79bef77e626c9ba60a9cde0a691835f18
chengfei001 <chengfei001@gmail.com>
2020-09-14 02:38:40 +0800

    server code initialize

---
+++ client/src/test/java/com/chengfei/client/ClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chengfei.client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
